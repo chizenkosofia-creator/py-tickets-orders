e3ee532d2ece46e764e34fae0b7196d4d0e1c477
Sofia <chizenko.sofia@gmail.com>
2026-08-27 18:46:09 +0300

    Change filter

--- cinema/views.py
+++ cinema/views.py
@@ -1,8 +1,6 @@
 from django.db.models import QuerySet
 from rest_framework import viewsets
 from rest_framework.serializers import Serializer
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
 from cinema.serializers import (
     GenreSerializer,
@@ -36,28 +34,49 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    pagination_class = None
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ["genres", "actors"]
-    search_fields = ["title"]
 
-    def get_queryset(self) -> QuerySet[Movie]:
-        queryset = self.queryset
-        if self.action in ("list", "retrieve"):
-            return queryset.prefetch_related("genres", "actors")
-        return queryset
+    @staticmethod
+    def parse_query_params(params: str | None) -> list[int] | None:
+        if params is None:
+            return None
+        return [int(id_) for id_ in params.split(",")]
 
-    def get_serializer_class(self) -> type[Serializer]:
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+
         if self.action == "list":
-            return MovieListSerializer
-        elif self.action == "retrieve":
-            return MovieRetrieveSerializer
+            serializer = MovieListSerializer
 
-        return MovieSerializer
+        if self.action == "retrieve":
+            serializer = MovieRetrieveSerializer
 
+        return serializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related("actors", "genres")
+            if self.action == "list":
+                actors = self.parse_query_params(
+                    self.request.query_params.get("actors")
+                )
+                if actors:
+                    queryset = queryset.filter(actors__id__in=actors)
+
+                genres = self.parse_query_params(
+                    self.request.query_params.get("genres")
+                )
+                if genres:
+                    queryset = queryset.filter(genres__id__in=genres)
+
+                title = self.request.query_params.get("title")
+                if title:
+                    queryset = queryset.filter(title__icontains=title)
+
+        return queryset.distinct()
 class MovieSessionViewSet(viewsets.ModelViewSet):
     pagination_class = None
     queryset = MovieSession.objects.all()
