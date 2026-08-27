9564b27210e3ad742c6db867eddfd1b2b28f1e59
Sofia <chizenko.sofia@gmail.com>
2026-08-27 19:31:17 +0300

    Fix8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,10 +3,38 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from cinema.models import (
+    Actor,
+    CinemaHall,
+    Genre,
+    Movie,
     MovieSession,
     Order,
     Ticket,
 )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "description", "duration", "genres", "actors")
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -67,6 +95,12 @@ class OrderSerializer(serializers.ModelSerializer):
 
 class OrderListSerializer(OrderSerializer):
     tickets = TicketListSerializer(many=True, read_only=True)
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = ("id", "show_time", "movie", "cinema_hall")
 
 
 class MovieSessionListSerializer(serializers.ModelSerializer):
